9f75b6cf51a9704ff66154e5b294ac2f8ca7d5a1
driksot <derricksouthworth@gmail.com>
2022-11-24 01:22:42 -0700

    Added more product models to ProductConfiguration

--- AgateMountainWarehouse.Infrastructure/Configurations/Entities/ProductConfiguration.cs
+++ AgateMountainWarehouse.Infrastructure/Configurations/Entities/ProductConfiguration.cs
@@ -28,6 +28,66 @@ public class ProductConfiguration : IEntityTypeConfiguration<Product>
                 ImageUrl = "images/products/turquoise.png",
                 CreatedDateTime = DateTime.Now,
                 UpdatedDateTime = DateTime.Now
+            },
+            new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = "Star Garnet",
+                Description = "Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
+                Price = 25,
+                ImageUrl = "images/products/star-garnet.png",
+                CreatedDateTime = DateTime.Now,
+                UpdatedDateTime = DateTime.Now
+            },
+            new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = "Jasper",
+                Description = "Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
+                Price = 20,
+                ImageUrl = "images/products/jasper.png",
+                CreatedDateTime = DateTime.Now,
+                UpdatedDateTime = DateTime.Now
+            },
+            new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = "Fire Opal",
+                Description = "Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
+                Price = 18,
+                ImageUrl = "images/products/fire-opal.png",
+                CreatedDateTime = DateTime.Now,
+                UpdatedDateTime = DateTime.Now
+            },
+            new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = "Amethyst",
+                Description = "Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus.",
+                Price = 22,
+                ImageUrl = "images/products/amethyst.png",
+                CreatedDateTime = DateTime.Now,
+                UpdatedDateTime = DateTime.Now
+            },
+            new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = "26 Gauge Gold Wire",
+                Description = "Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus.",
+                Price = 9,
+                ImageUrl = "images/products/26-gauge-gold-wire.png",
+                CreatedDateTime = DateTime.Now,
+                UpdatedDateTime = DateTime.Now
+            },
+            new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = "24 Gauge Gold Wire",
+                Description = "Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
+                Price = 8,
+                ImageUrl = "images/products/24-gauge-gold-wire.png",
+                CreatedDateTime = DateTime.Now,
+                UpdatedDateTime = DateTime.Now
             });
     }
 }
