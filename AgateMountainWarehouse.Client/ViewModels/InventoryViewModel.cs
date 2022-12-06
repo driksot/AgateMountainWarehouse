220dc5975e022fc7a304c3f260953f5d20f6a2d6
driksot <derricksouthworth@gmail.com>
2022-12-05 19:50:27 -0700

    Add client validations to forms

--- AgateMountainWarehouse.Client/ViewModels/InventoryViewModel.cs
+++ AgateMountainWarehouse.Client/ViewModels/InventoryViewModel.cs
@@ -1,10 +1,15 @@
-﻿namespace AgateMountainWarehouse.Client.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgateMountainWarehouse.Client.ViewModels
 {
     public class InventoryViewModel
     {
         public Guid Id { get; set; }
         public Guid ProductId { get; set; }
         public ProductViewModel? Product { get; set; }
+
+        [Required(ErrorMessage = "Quantity is a required field")]
+        [Range(0, int.MaxValue, ErrorMessage = "The quantity can't be lower than 0")]
         public int QuantityOnHand { get; set; }
     }
 }
