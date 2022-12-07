c7ecbd45fbb1f8a75c383b578118fb8402c02dc5
driksot <derricksouthworth@gmail.com>
2022-12-06 19:33:21 -0700

    Add start of Blazor Identity

--- AgateMountainWarehouse.Client/Program.cs
+++ AgateMountainWarehouse.Client/Program.cs
@@ -1,6 +1,8 @@
 using AgateMountainWarehouse.Client;
+using AgateMountainWarehouse.Client.AuthProviders;
 using AgateMountainWarehouse.Client.HttpRepository;
 using AgateMountainWarehouse.Client.Static;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Tewr.Blazor.FileReader;
@@ -14,7 +16,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
     builder.Services.AddScoped<IProductHttpRepository, ProductHttpRepository>();
     builder.Services.AddScoped<IInventoryHttpRepository, InventoryHttpRepository>();
     builder.Services.AddScoped<IOrderHttpRepository, OrderHttpRepository>();
+    builder.Services.AddScoped<AuthenticationStateProvider, TestAuthStateProvider>();
 
+    builder.Services.AddAuthorizationCore();
     builder.Services.AddFileReaderService(o => o.UseWasmSharedBuffer = true);
 }
 
